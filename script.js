b6d064606f31e68e2b7ab6c500cbd9ed8a0b8d01
David <dwarrenfive@gmail.com>
2020-12-16 19:10:08 -0800

    adds button, streak counter, random logic

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ let shellImg3 = document.getElementById("seashell3");
 let piratePath = "assets/images/pirate.png";
 let chestPath = "assets/images/chest.png";
 let pearlPath = "assets/images/pearl.png";
+let numShells = 3;
+let openShell1
+let openShell2
+let openShell3
+
+const randomPiratePosition = () => {
+  let piratePosition = Math.floor(Math.random() * numShells)
+  // console.log(piratePosition)
+}
 
 shellImg1.onclick = () => {
   shellImg1.src = piratePath;
