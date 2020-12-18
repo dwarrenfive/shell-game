bb9393c7a277224233f4494fcfc5b0d2d868df22
David <dwarrenfive@gmail.com>
2020-12-18 14:22:42 -0800

    adds random position and decremented possiblilty logic

--- script.js
+++ script.js
@@ -5,23 +5,53 @@ let piratePath = "assets/images/pirate.png";
 let chestPath = "assets/images/chest.png";
 let pearlPath = "assets/images/pearl.png";
 let numShells = 3;
-let openShell1
-let openShell2
-let openShell3
+let openShell1;
+let openShell2;
+let openShell3;
+let closedShell = "assets/images/closed_seashell.png";
+
+document.body.onload = function () {
+  randomPiratePosition();
+};
+
+const isClicked = (shell) => {};
+
+const numOfPlays = () => {
+  numShells--;
+  if (numShells === 0) {
+    console.log("Game Over!");
+    // gameOver();
+  }
+};
 
 const randomPiratePosition = () => {
-  let piratePosition = Math.floor(Math.random() * numShells)
-  // console.log(piratePosition)
-}
+  let piratePosition = Math.floor(Math.random() * numShells);
+  if (piratePosition === 0) {
+    openShell1 = piratePath;
+    openShell2 = chestPath;
+    openShell3 = pearlPath;
+  } else if (piratePosition === 1) {
+    openShell2 = piratePath;
+    openShell1 = chestPath;
+    openShell3 = pearlPath;
+  } else {
+    openShell3 = piratePath;
+    openShell1 = chestPath;
+    openShell2 = pearlPath;
+  }
+};
 
 shellImg1.onclick = () => {
-  shellImg1.src = piratePath;
+  shellImg1.src = openShell1;
+  numOfPlays();
 };
 
 shellImg2.onclick = () => {
-  shellImg2.src = chestPath;
+  shellImg2.src = openShell2;
+  numOfPlays();
 };
 
 shellImg3.onclick = () => {
-  shellImg3.src = pearlPath;
+  shellImg3.src = openShell3;
+  numOfPlays();
 };
